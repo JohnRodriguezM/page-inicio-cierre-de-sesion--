0096e55660f53c9991c10a237db75cbaeda53a79
John Jairo Rodriguez Medina <johnjairorodriguez384@gmail.com>
2021-12-06 23:25:39 -0500

    a

--- js/index.mjs
+++ js/index.mjs
@@ -1,9 +1,25 @@
-import {firebaseConfig,app} from "./firebase.mjs"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIoouw-AJ8HFwcDk22D4oquQ-OufuHYtw",
+  authDomain: "crud-baf3e.firebaseapp.com",
+  databaseURL: "https://crud-baf3e-default-rtdb.firebaseio.com",
+  projectId: "crud-baf3e",
+  storageBucket: "crud-baf3e.appspot.com",
+  messagingSenderId: "819463359747",
+  appId: "1:819463359747:web:0ae98b65b3d5eb77692d36"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
-console.log(app)
-
-
+/*
+codigo sin firebase, perfecto
 
 function envioForm(e) {
     e.preventDefault(e)
@@ -17,19 +33,16 @@ function envioForm(e) {
         edad,
         comentariosH,
     }
-    envio(datos)
-    envioForm.reset()
-    alert(datos)
+    
+    document.getElementById("formularioEnvio").reset()
+    alert("enviado")
+    
 
 }
 const envioFormm = document.getElementById("formularioEnvio").addEventListener('submit',envioForm);
 
-function envio(datos){
-    firebase.database().ref('contacto').push(datos)
-        .then(function(){
-            alert("guardado")
-        })
-        .catch(function(){
-            alert("no guardado")
-        })
-};
+
+
+*/
+
+
