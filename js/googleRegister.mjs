be095f6660c4ef89a6b476998af3416e6d395af1
John Jairo Rodriguez Medina <johnjairorodriguez384@gmail.com>
2021-12-19 23:32:02 -0500

    google ok

--- js/googleRegister.mjs
+++ js/googleRegister.mjs
@@ -4,7 +4,7 @@ function envioGoogle(a) {
     a.preventDefault();
     const envio = new autenticacion();
     envio.google();
- 
+    window.location.href = "../html/envioDatos.html"
  }
 
  document.getElementById('buttonGoogle').addEventListener('click',envioGoogle)
