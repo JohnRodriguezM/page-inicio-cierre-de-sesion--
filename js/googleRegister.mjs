e246a87e0e2aa78790f98618961e3d026d8d3429
John Jairo Rodriguez Medina <johnjairorodriguez384@gmail.com>
2021-12-19 23:44:58 -0500

    prueba para redireccion

--- js/googleRegister.mjs
+++ js/googleRegister.mjs
@@ -4,7 +4,6 @@ function envioGoogle(a) {
     a.preventDefault();
     const envio = new autenticacion();
     envio.google();
-    window.location.href = "../html/envioDatos.html"
  }
 
  document.getElementById('buttonGoogle').addEventListener('click',envioGoogle)
